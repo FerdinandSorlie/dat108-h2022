e627b9c22ddcb612ab5a70dc57899cb1f0a40e88
FerdinandSorlie <ferdinandaugust.sorlie@outlook.com>
2022-09-19 17:38:40 +0200

    min eddit

--- F09 - Last ned til timen/src/no/hvl/dat108/f09/Eksempel3.java
+++ F09 - Last ned til timen/src/no/hvl/dat108/f09/Eksempel3.java
@@ -18,16 +18,26 @@ public class Eksempel3 {
 				);
 		
 		//Alle forbokstavene i fornavnene i en streng - reduce "CLTCM"
+		String initialer = people.stream()
+			.map(p -> "" + p.getFirstName().charAt(0))
+			.reduce("", String::concat);
+		System.out.println(initialer);
 		
 		//Antall personer - count
-		
+		long antallPersoner = people.stream().filter(p -> p.getAge()>50).count();
+		System.out.println(antallPersoner);
 		//Om vi har data som matcher - anyMatch, allMatch, noneMatch
 		//Er alle over 30 år?
+		boolean alleOver = people.stream().allMatch(p -> p.getAge()>30);
+		System.out.println("alle over 30?" + alleOver);
 		//Er noen over 60 år?
+		boolean noenOver = people.stream().anyMatch(p -> p.getAge()>60);
+		System.out.println("noen over 60?" + noenOver);
 
 		//Summen av alle tall i [1..100]
 		
 		//Det minste tallet i streamen ...
+		// ... min() 
 
 		//Finn en som er 30 eller yngre
 		
